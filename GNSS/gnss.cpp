c749f00142c3235a492a807dfec178f9a8969783
GumUlf <programming@gumulf.se>
2021-03-07 00:34:51 +0100

    Added more constructors and funtions to Position class and added classes for satelite information and fix type and quality. Also added checks and error handling

--- GNSS/gnss.cpp
+++ GNSS/gnss.cpp
@@ -1,44 +1,170 @@
 #include "gnss.h"
 
+
 namespace gnss{
+
+	/* Points in space time :) */
 
 	Position::Position(): m_latitude{0.0}, m_longitude{0.0}{}
 
 	Position::Position(double latitude, double longitude){
 
-		if(latitude >= -90.0 && latitude <= 90.0 && longitude >= -180.0 && longitude <= 180.0){
-			m_latitude = latitude;
-			m_longitude = longitude;
-		} else{
-			// TODO Create error
-		}
-
-	}
-
-	double Position::getLatitude(){
-		return m_latitude;
-	}
-	double Position::getLongitude(){
-		return m_longitude;
-	}
-
-	void Position::setLatitude(double latitude){
 		if(latitude >= -90.0 && latitude <= 90.0){
 			m_latitude = latitude;
 		} else{
-			// TODO Create Error
+			throw(std::invalid_argument("Latitude must have a value between -90.0 and 90.0"));
 		}
-	
-	}
-	void Position::setLongitude(double longitude){
+
 		if(longitude >= -180.0 && longitude <= 180.0){
 			m_longitude = longitude;
 		} else{
-			// TODO Create Error
+			throw(std::invalid_argument("Longitude must have a value between - 180.0 - 180.0"));
 		}
+	}
+
+	Position::Position(double latitude, double longitude, tm utc_time): 
+		Position::Position(latitude, longitude){
+
+		m_utc_time = utc_time;
+	
+	}
+	
+	Position::Position(double latitude, double longitude, double meters_over_msl, double meters_over_ellipsoid):
+		Position::Position(latitude, longitude){
+
+		m_meters_over_msl = meters_over_msl;
+		m_meters_over_ellipsoid = meters_over_ellipsoid;
+
+	}
+
+	Position::Position(double latitude, double longitude, double meters_over_msl, double meters_over_ellipsoid, tm utc_time):
+		Position::Position(latitude, longitude, meters_over_msl, meters_over_ellipsoid) {
+	
+		m_utc_time = utc_time;
 	
 	}
 
+	double Position::getLatitude(){ return m_latitude; }
+	double Position::getLongitude(){ return m_longitude; }
+	double Position::getMetersOverMsl(){ return m_meters_over_msl; }
+	double Position::getMetersOverEllipsoid(){ return m_meters_over_ellipsoid; }
+	tm Position::getUtcTime(){ return m_utc_time; }
 
+	void Position::setLatitude(double latitude){
+
+		if(latitude >= -90.0 && latitude <= 90.0){
+			m_latitude = latitude;
+		} else{
+			throw(std::invalid_argument("Values outside allowed range, -90.0 - 90.0"));
+		}
+	}
+	void Position::setLongitude(double longitude){
+
+		if(longitude >= -180.0 && longitude <= 180.0){
+			m_longitude = longitude;
+		} else{
+			throw(std::invalid_argument("Values outside allowed range, -180.0 - 180.0"));
+		}
+	}
+
+
+	void Position::setMetersOverMsl(double meters_over_msl){ m_meters_over_msl = meters_over_msl; }
+	void Position::setMetersOverEllipsoid(double meters_over_ellipsoid){ m_meters_over_ellipsoid = meters_over_ellipsoid; }
+	void Position::setUtcTime(tm utc_time){ m_utc_time = utc_time; }
+
+
+	/* Satelites */
+
+	std::set<int> Satelite::m_ids{};
+
+	Satelite::Satelite(unsigned int id){
+
+		if(m_ids.count(id) == 0){
+			m_id = id;
+			m_ids.insert(id);
+		} else{
+			throw std::invalid_argument("ID does already exist");
+		}
+	}
+
+	Satelite::Satelite(unsigned int id, unsigned int elevation, unsigned int azimuth, unsigned int snr){
+
+		if(m_ids.count(id) == 0){
+			m_id = id;
+			m_ids.insert(id);
+		} else{
+			throw std::invalid_argument("ID does already exist");
+		}
+
+		if(elevation <= 90){
+			m_elevation = elevation;
+		} else{
+			throw std::invalid_argument("Elevation must be less than or equal to 90");
+		}
+
+		if(azimuth < 360){
+			m_azimuth = azimuth;
+		} else{
+			throw std::invalid_argument("Azumuth must be less than 360");
+		}
+
+		if(snr < 100){
+			m_snr = snr;
+		} else{
+			throw std::invalid_argument("SNR must be lower than 100");
+		}
+	}
+
+	unsigned int Satelite::getId(){ return m_id; }
+	unsigned int Satelite::getElevation(){ return m_elevation; }
+	unsigned int Satelite::getAzimuth(){ return m_azimuth; }
+	unsigned int Satelite::getSnr(){ return m_snr; }
+
+	void Satelite::setElevation(unsigned int elevation){
+
+		if(elevation <= 90){
+			m_elevation = elevation;
+		} else{
+			throw std::invalid_argument("Elevation must be less than or equal to 90");
+		}
+	}
+
+	void Satelite::setAzimuth(unsigned int azimuth){
+
+		if(azimuth < 360){
+			m_azimuth = azimuth;
+		} else{
+			throw std::invalid_argument("Azumuth must be less than 360");
+		}
+	}
+
+	void Satelite::setSnr(unsigned int snr){
+
+		if(snr < 100){
+			m_snr = snr;
+		} else{
+			throw std::invalid_argument("SNR must be lower than 100");
+		}
+	}
+
+	FixQuality::FixQuality():
+		m_fix_mode{FixType::invalid}, m_pdop{-1.0}, m_hdop{-1.0}, m_vdop{-1.0} {}
+
+	FixQuality::FixQuality(FixType fix_mode, double pdop, double hdop, double vdop):
+		m_fix_mode{fix_mode}, m_pdop{pdop}, m_hdop{hdop}, m_vdop{vdop} {}
+
+	void FixQuality::setQuality(FixType fix_mode, double pdop, double hdop, double vdop) { 
+
+		m_fix_mode = fix_mode;
+		m_pdop = pdop;
+		m_hdop = hdop;
+		m_vdop = vdop;
+
+	}
+
+	FixQuality::FixType FixQuality::getFixMode(){ return m_fix_mode; }
+	double FixQuality::getPdop(){ return m_pdop; }
+	double FixQuality::getHdop(){ return m_hdop; }
+	double FixQuality::getVdop(){ return m_vdop; }
 
 }
--- GNSS/gnss.h
+++ GNSS/gnss.h
@@ -1,4 +1,7 @@
 #pragma once
+#include<set>
+#include<stdexcept>
+#include<ctime>
 
 // TODO Implement class and functions
 // Should include datatypes for position, time, satelite data et.c.
@@ -9,20 +12,30 @@ namespace gnss{
 
 		double m_latitude{0.0};   // North-South	(90.0 - -90.0 degrees)
 		double m_longitude{0.0};  // East-West	(180.0 - -180.0 degrees)
-		double m_metersOverMsl{0.0};  // Hight over mean sealevel
-		double m_metersOverEllipsoid{0.0}; // Hight over Ellipsoid
+		double m_meters_over_msl{0.0};  // Hight over mean sealevel
+		double m_meters_over_ellipsoid{0.0}; // Hight over Ellipsoid
+		tm m_utc_time{NULL};
 
 	public:
-
 		Position();
-
 		Position(double latitude, double longitude);
+		Position(double latitude, double longitude, tm utc_time);
+		Position(double latitude, double longitude, double meters_over_msl, double meters_over_ellipsoid);
+		Position(double latitude, double longitude, double meters_over_msl, double meters_over_ellipsoid, tm utc_time);
 
 		double getLatitude();
 		double getLongitude();
+		double getMetersOverMsl();
+		double getMetersOverEllipsoid();
+		tm getUtcTime();
 
 		void setLatitude(double latitude);
 		void setLongitude(double longitude);
+		void setMetersOverMsl(double meters_over_msl);
+		void setMetersOverEllipsoid(double meters_over_ellipsoid);
+		void setUtcTime(tm utc_time);
+
+
 
 	};
 
@@ -30,9 +43,54 @@ namespace gnss{
 	// Time
 
 	// Satelite data type
+	class Satelite{
 
-	// Fix type
+		unsigned int m_id{0};
+		unsigned int m_elevation{0};
+		unsigned int m_azimuth{0};
+		unsigned int m_snr{0};
+		static std::set<int> m_ids;
 
-	// Quality (Struct with the different quality types)
+	public:
+		Satelite(unsigned int id);
+		Satelite(unsigned int id, 
+					unsigned int elevation, 
+					unsigned int azimuth, 
+					unsigned int snr);
+
+		unsigned int getId();
+		unsigned int getElevation();
+		unsigned int getAzimuth();
+		unsigned int getSnr();
+
+		void setElevation(unsigned int elevation);
+		void setAzimuth(unsigned int azimuth);
+		void setSnr(unsigned int snr);
+
+	};
+
+	// Quality and Fix type
+
+	class FixQuality{
+
+	public:
+		enum class FixType{ invalid, valid_2D = 2, valid_3D = 3 };
+
+	private:
+		FixType m_fix_mode{FixType::invalid};
+		double m_pdop{-1.0};
+		double m_hdop{-1.0};
+		double m_vdop{-1.0};
+
+	public:
+		FixQuality();
+		FixQuality(FixType fix_mode, double pdop, double hdop, double vdop);
+		void setQuality(FixType fix_mode, double pdop, double hdop, double vdop);
+		FixType getFixMode();
+		double getPdop();
+		double getHdop();
+		double getVdop();
+		
+	};
 
 }
