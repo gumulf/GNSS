f891bab0fa4dc3bd1f90922955197c60393e0d86
GumUlf <programming@gumulf.se>
2021-03-18 03:25:18 +0100

    Testing error recovery, and limiting retries

--- GNSS/main.cpp
+++ GNSS/main.cpp
@@ -31,57 +31,87 @@ int main(){
 
 	gnss::Nmea nmea{};
 
-	bool error_occured_reading_data{false};
-	int round{0}, no_of_rounds{100};
+	bool reading_data_ok{false};
+	bool reconnect{false};
+	int lines_read{0}, lines_to_read{0}; // lines_to_read = 0 => no limit, Ctrl+C to stop
+	int retries_opening_port{0}, max_retries_opening_port{10};
+	int retries_reading_line{0}, max_retries_reading_line{10};
+	int retries_reconnecting{0}, max_retries_reconnecting{100};
 
 	do{
-		bool error_occured_opening_port{false};
+		reconnect = false;
+		if(retries_reconnecting++ > max_retries_reconnecting){
+			std::cerr << "Maxium retries reconnecting reached, aborting!" << std::endl;
+			return -1;
+		}
+
+		bool port_opened_ok{false};
 
 		do{
+			if(retries_opening_port++ > max_retries_opening_port){
+				std::cerr << "Maximum retries reached while trying to open port, aborting!" << std::endl;
+				return -1;
+			}
 			try{
 				port_handle = gnss::openPort(port);
-				error_occured_opening_port = false;
+				port_opened_ok = true;
 			}
 			catch(gnss::CommError){
-				error_occured_opening_port = true;
+				port_opened_ok = false;
 				gnss::closePort(port_handle);
 				Sleep(1000);
 			}
-		} while(error_occured_opening_port);
+		} while(!port_opened_ok);
 
 
-		while(round < no_of_rounds){
-			// Read data from "GPS"
+		// Read data from "GPS"	
+		while((lines_read < lines_to_read || lines_to_read == 0) && !reconnect){
 
-			std::string line{};
+			retries_reading_line = 0;
 
-			try{
-				line = gnss::readLineCrLf(port_handle);
-				error_occured_reading_data = false;
-			}
-			catch(gnss::CommError){
-				error_occured_reading_data = true;
-				gnss::closePort(port_handle);
-			}
+			while(!reading_data_ok && !reconnect){
+				std::string line{};
 
-			if(!error_occured_reading_data){
-
-				std::cout << "Sentence: \"" << line << "\"\n";
-	
-				// Parse data	
-				if(nmea.nmeaParser(line)){
-					std::cout << "OK!\n";
-				} else{
-					std::cout << "Not OK!\n";
+				try{
+					line = gnss::readLineCrLf(port_handle);
+					reading_data_ok = true;
 				}
-				std::cout << "\n\n";
+				catch(gnss::CommError){
+					reading_data_ok = false;
+				}
+
+				if(reading_data_ok){
+
+					++lines_read;
+					retries_reading_line = 0;
+					retries_reconnecting = 0;
+
+
+					// Parse data	
+					if(nmea.nmeaParser(line)){
+						std::cout << "Parsed OK:   ";
+						std::cout << line << "\n";
+					}
+					else{
+						std::cout << "Not parsed:  ";
+						std::cout << line << "\n";
+					}
+					reading_data_ok = false;
+
+				}
+				else{
+					if(++retries_reading_line >= max_retries_reading_line){
+						gnss::closePort(port_handle);
+						reconnect = true;
+						Sleep(1000);
+					}
+				}
 			}
-
-			++round;
 		}
-	} while(error_occured_reading_data);
+	} while(reconnect && (lines_read < lines_to_read || lines_to_read == 0));
 
-	// Update position data
+
+	// Update(/get?) position data
 
 	// Present position data
 
