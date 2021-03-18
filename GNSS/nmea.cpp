5cde2aa15968739e6110829e2db21b35d8eb6c93
GumUlf <programming@gumulf.se>
2021-03-18 03:24:02 +0100

    Started moving out reuseable code to separate functions.  Fixed bugs in validateChecksum-function.

--- GNSS/nmea.cpp
+++ GNSS/nmea.cpp
@@ -11,38 +11,32 @@ namespace gnss{
 	// Function declarations
 
 	bool validateChecksum(std::string sentence);
-	std::vector<std::string>  tokenize(std::string str, std::string separators);
+	std::vector<std::string>  tokenize(std::string str, std::string start_chars, std::string separators);
 	bool parseGga(std::vector<std::string> sentence_tokens, gnss::Position &position, gnss::FixQuality &fix_quality);
+	tm extractTime(std::string time_string);
+	double extractCoordinate(std::string coordinate_string, double lower_limit, double upper_limit);
 
 	// Parser
 	// TODO Define and implement how data is returned/updated
 
 	bool Nmea::nmeaParser(std::string sentence){
 
-		if(!validateChecksum(sentence)){ return false; }
+		if(!validateChecksum(sentence)){
+			return false;
+		}
 
-		std::vector<std::string> sentence_tokens{tokenize(sentence, ",*")};
+		std::vector<std::string> sentence_tokens{tokenize(sentence, "$!", ",*")};
 
 		std::string sentenceType{sentence_tokens.front().substr(3, 3)};
 
-		std::cout << "The sentence type is: " << sentenceType << "\n";
-
 		if(sentenceType == "GGA"){
-			if(parseGga(sentence_tokens, m_position, m_fix_quality)){
-				std::cout << "Parsing of GGA done OK\n";
-			} else{
-				std::cout << "Could not parse GGA\n";
-			}
-
-		} else{
-			std::cout << "Parsing of type " << sentence_tokens.at(0) << " in not implemented!\n";
-
-			// Unkown sentence type
-			// TODO Do good stuff here
+			return parseGga(sentence_tokens, m_position, m_fix_quality);
+		}
+		else{
+			// std::cout << "Parsing of type " << sentence_tokens.at(0) << " in not implemented!\n";
+			return false;
 		}
 
-		return true;
-		
 	}
 
 	gnss::Position Nmea::getPosition(){ return m_position; }
@@ -59,42 +53,38 @@ namespace gnss{
 
 				Position current_position{};
 				tm current_utc_time{};
-				bool valid_utc_time{false};
 
-				int hour{std::stoi(sentence_tokens.at(1).substr(0, 2))};
-				int minute{std::stoi(sentence_tokens.at(1).substr(2, 2))};
-				int second{std::stoi(sentence_tokens.at(1).substr(4, 2))};
-
-				if(hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59 && second >= 0 && second <= 60){
-					current_utc_time.tm_hour = hour;
-					current_utc_time.tm_min = minute;
-					current_utc_time.tm_sec = second;
-					current_position.setUtcTime(current_utc_time);
-					valid_utc_time = true;
+				try{
+					current_utc_time = extractTime(sentence_tokens.at(1));
 				}
-
-				bool valid_position{true};
-
-				double latitude{std::stod(sentence_tokens.at(2).substr(0, 2)) + (std::stod(sentence_tokens.at(2).substr(2, std::string::npos)) / 60.0)};
-				double longitude{std::stod(sentence_tokens.at(4).substr(0, 3)) + (std::stod(sentence_tokens.at(4).substr(3, std::string::npos)) / 60.0)};
-
-				if(latitude >= -90.0 && latitude <= 90.0 && longitude >= -180.0 && longitude <= 180.0){
-					current_position.setLatitude(latitude);
-					current_position.setLongitude(longitude);
-				} else{
-					valid_position = false;
+				catch(std::out_of_range){
+					return false;
 				}
+				catch(...){ throw; }
+
+				current_position.setUtcTime(current_utc_time);
+
+				try{
+					double latitude{extractCoordinate(sentence_tokens.at(2), -90.0, 90.0)};
+					double longitude{extractCoordinate(sentence_tokens.at(4), -180.0, 180.0)};
+				}
+				catch(std::out_of_range){
+					return false;
+				}
+				catch(std::invalid_argument){
+					return false;
+				}
+				catch(...){ throw; }
 
 				double altitudeMsl{std::stod(sentence_tokens.at(9))};
-
-				bool valid_altitudes{true};
 
 				// This should not be anything other than in meters, but just in case
 				if(sentence_tokens.at(10) == "M" || sentence_tokens.at(10) == "m"){
 					current_position.setMetersOverMsl(altitudeMsl);
 
-				} else{
-					valid_altitudes = false;
+				}
+				else{
+					return false;
 				}
 
 				double altitudeEllipsoid{std::stod(sentence_tokens.at(9)) + std::stod(sentence_tokens.at(11))};
@@ -102,29 +92,23 @@ namespace gnss{
 				// This should not be anything other than in meters, but just in case
 				if(sentence_tokens.at(12) == "M" || sentence_tokens.at(12) == "m"){
 					current_position.setMetersOverEllipsoid(altitudeEllipsoid);
-				} else{
-					valid_altitudes = false;
 				}
+				else{
+					return false;
+				}
+
+				position = current_position;
 
 				fix_quality.setFixQuality(
 					gnss::FixQuality::FixType::valid,
-					static_cast<unsigned int>(std::stoi(sentence_tokens.at(7))),
-					std::stod(sentence_tokens.at(8)));
+					static_cast<unsigned int>(std::stoi(sentence_tokens.at(7))), // Satellites used for fix
+					std::stod(sentence_tokens.at(8))); // hdop
+
+				return true;
 
 
-				if(valid_position && valid_altitudes && valid_utc_time){
-					position = current_position;
-					fix_quality.setFixQuality(
-						gnss::FixQuality::FixType::valid,
-						static_cast<unsigned int>(std::stoi(sentence_tokens.at(7))),
-						std::stod(sentence_tokens.at(8)));
-
-					return true;
-				}
-
-				return false;
-
-			} else{
+			}
+			else{
 				return false; // Fixation not available!
 			}
 
@@ -146,28 +130,32 @@ namespace gnss{
 	bool validateChecksum(std::string sentence){
 
 		// Sentence should start with '$' or '!'.
-		if(sentence.find_first_of("$!") != 0){
+		size_t firstChar{sentence.find_first_of("$!")};
+
+		//	If there is no '$' or '!', the test fails
+		if(firstChar == std::string::npos){
 			return false;
 		}
 
-		// The data is between first character, checked above, and '*'.
-		size_t sentenceEnd{sentence.find('*')};
+		// The data is between first character, checked above, and '*',
+		// and the checksum is after the '*'.
 
-		// There should always be an '*' between data and checksum
-		if(sentenceEnd == std::string::npos){
+		size_t sentenceEnd{sentence.find('*')};
+		if(sentenceEnd == std::string::npos || firstChar > sentenceEnd){
 			return false;
 		}
 
 		// Calculation of checksum
 		unsigned char checksum{0};
-		for(int pos{1}; pos < sentenceEnd; ++pos){
+		for(size_t pos{firstChar + 1}; pos < sentenceEnd; ++pos){
 			checksum ^= sentence.at(pos);
 		}
 
 		// Compare calculated checksum against checksum at end of sentence (that is in hexadecimal notation)
 		if(std::stoi(sentence.substr(sentenceEnd + 1, std::string::npos), nullptr, 16) == static_cast<int>(checksum)){
 			return true;
-		} else{
+		}
+		else{
 			return false;
 		}
 
@@ -175,10 +163,10 @@ namespace gnss{
 
 	// Tokenize the sentence
 
-	std::vector<std::string>  tokenize(std::string str, std::string separators){
+	std::vector<std::string>  tokenize(std::string str, std::string start_chars, std::string separators){
 
 		std::vector<std::string> tokens;
-		size_t start{0}, end{str.find_first_of(separators, start)};
+		size_t start{str.find_first_of(start_chars, 0)}, end{str.find_first_of(separators, start)};
 
 		while(end != std::string::npos){
 			tokens.push_back(str.substr(start, end - start));
@@ -193,4 +181,48 @@ namespace gnss{
 		return tokens;
 	}
 
+	// Get time from string, with checking
+	tm extractTime(std::string time_string){
+
+		int hour{std::stoi(time_string.substr(0, 2))};
+		int minute{std::stoi(time_string.substr(2, 2))};
+		int second{std::stoi(time_string.substr(4, 2))};
+
+
+
+		if(hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59 && second >= 0 && second <= 60){
+
+			tm time_object{};
+			time_object.tm_hour = hour;
+			time_object.tm_min = minute;
+			time_object.tm_sec = second;
+
+			return time_object;
+
+		}
+		else{
+			throw std::out_of_range("");
+		}
+
+	}
+
+	// Get coordinate, with checking
+	double extractCoordinate(std::string coordinate_string, double lower_limit, double upper_limit){
+
+		size_t decimalPosition{coordinate_string.find('.')};
+
+		if(decimalPosition == std::string::npos){
+			throw std::invalid_argument("No decimal point!");
+		}
+
+		double coordinate{0};
+		coordinate += std::stod(coordinate_string.substr(0, decimalPosition - 2));
+		coordinate += (std::stod(coordinate_string.substr(decimalPosition - 2, std::string::npos)) / 60.0);
+
+		if(coordinate < lower_limit && coordinate > upper_limit){
+			throw std::out_of_range("Coordinates are out of range!");
+		}
+
+		return coordinate;
+	}
 }
